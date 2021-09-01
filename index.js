ca86ee11738ded44dbc203c5483f3b2e63df3485
devtot0 <33835148+devtot0@users.noreply.github.com>
2021-09-01 16:23:46 +0200

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { response, request } = require("express");
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 let persons = [
